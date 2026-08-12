1d6c5542456d271383fe754b56930d092c1f684d
aidbrm <idbrm16@gmail.com>
2026-08-12 17:55:51 +0100

    handled some edge cases and advance on the algo

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 import sys
+import re
 
 
 class ParsingError(Exception):
@@ -31,9 +32,32 @@ class Parsing():
         if not data:
             print("ERROR: the file content only comment and blank no data")
             sys.exit(1)
+
+        name, number = data[0].split(":")
+
+        if not name == "nb_drones":
+            raise ParsingError("the file should start with nb_drones")
+
+        number = number.strip()
+
+        if not number:
+            raise ParsingError("nb_drones is missing a value")
+
+        if not number.isdigit():
+            raise ParsingError(f"nb_drones value must be an integer, got '{number}'")
+
+        nb = int(number)
+        if nb > 10000:
+            raise ParsingError(f"the number is too large max is 10000 got '{nb}'")
+        if nb < 0 or nb == 0:
+            raise ParsingError(f"the nuber should be bigger than 0")
+
         return data
 
-path = "maps/challenger/01_the_impossible_dream.txt"
-test = Parsing()
-print(test.read_file(path))
+try: 
+    path = "maps/challenger/01_the_impossible_dream.txt"
+    test = Parsing()
+    print(test.read_file(path))
+except ParsingError as e:
+    print(e)
 
